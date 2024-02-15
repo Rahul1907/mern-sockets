ced8a8a0a36c8924a41c54d2cba00ee8a88fa4c1
rahul1907 <rahul.19797@gmail.com>
2024-02-15 23:58:04 +0530

    Chats List and Search User Ui done

--- frontend/src/Components/Miscellaneous/ProfileModal.js
+++ frontend/src/Components/Miscellaneous/ProfileModal.js
@@ -1,7 +1,76 @@
 import React from "react";
+import { useDisclosure } from "@chakra-ui/hooks";
+import {
+  Button,
+  IconButton,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+import { ViewIcon } from "@chakra-ui/icons";
 
-const ProfileModal = () => {
-  return <div></div>;
+const ProfileModal = ({ user, children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      {children ? (
+        <span onClick={onOpen}> {children} </span>
+      ) : (
+        <IconButton
+          display={{ base: "flex" }}
+          icon={<ViewIcon />}
+          onClick={onOpen}
+        />
+      )}
+      <Modal size="lg" isCentered isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent height={"410px"}>
+          <ModalHeader
+            fontSize={"40px"}
+            fontFamily={"Roboto"}
+            display={"flex"}
+            justifyContent={"center"}
+          >
+            {user.name}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            display={"flex"}
+            flexDir={"column"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Image
+              borderRadius={"full"}
+              boxSize={"150px"}
+              src={user.pic}
+              alt={user.name}
+              margin={"auto"}
+            />
+            <Text
+              textAlign={"center"}
+              fontSize={{ base: "28px", md: "30px" }}
+              fontFamily={"Roboto"}
+            >
+              Email: {user.email}{" "}
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default ProfileModal;
--- frontend/src/Components/Miscellaneous/SideDrawer.js
+++ frontend/src/Components/Miscellaneous/SideDrawer.js
@@ -2,25 +2,108 @@ import {
   Avatar,
   Box,
   Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Input,
   Menu,
   MenuButton,
   MenuDivider,
   MenuItem,
   MenuList,
+  Spinner,
   Text,
   Tooltip,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { ChatState } from "../../Context/ChatProvider";
+import ProfileModal from "./ProfileModal";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import UserListItem from "../UserAvtar/UserListItem";
+import ChatLoading from "../ChatLoading";
 
 const SideDrawer = () => {
+  const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [chatLoading, setChatLoading] = useState(false);
 
-  const { user } = ChatState();
+  const { user, setSelectedChat, chats, setChats } = ChatState();
+
+  const toast = useToast();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/");
+  };
+
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: "Please enter something in search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      return;
+    }
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  const accessChat = async (userId) => {
+    try {
+      setChatLoading(true);
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.post("api/chat", { userId }, config);
+
+      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
+
+      setSelectedChat(data);
+      setChatLoading(false);
+      onClose();
+    } catch (error) {
+      setChatLoading(false);
+      toast({
+        title: "Error fetching data",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
 
   return (
     <>
@@ -34,7 +117,7 @@ const SideDrawer = () => {
         borderWidth={"5px"}
       >
         <Tooltip label="Search user to Chat" hasArrow placement="bottom-end">
-          <Button variant={"ghost"}>
+          <Button variant={"ghost"} onClick={onOpen}>
             <i className="fas fa-search"></i>
             <Text display={{ base: "none", md: "flex" }} px="4">
               Search User{" "}
@@ -61,13 +144,63 @@ const SideDrawer = () => {
               />
             </MenuButton>
             <MenuList>
-              <MenuItem> My Profile </MenuItem>
+              <ProfileModal user={user}>
+                <MenuItem> My Profile </MenuItem>
+              </ProfileModal>
               <MenuDivider />
-              <MenuItem> Logout </MenuItem>
+              <MenuItem onClick={logoutHandler}> Logout </MenuItem>
             </MenuList>
           </Menu>
         </Box>
       </Box>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Box display={"flex"} pb="2">
+              <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+            {loading ? (
+              <ChatLoading />
+            ) : (
+              searchResult.map((user) => {
+                return (
+                  <UserListItem
+                    key={user._id}
+                    user={user}
+                    handleFunction={() => {
+                      accessChat(user._id);
+                    }}
+                  />
+                );
+              })
+            )}
+          </DrawerBody>
+          {chatLoading && <Spinner ml="auto" display={"flex"} />}
+          {/* <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter> */}
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
